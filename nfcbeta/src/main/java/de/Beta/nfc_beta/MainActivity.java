84cc5ce0f4fa5aae3da36f4396f3e06616bd023f
MarlonDragun <marlon@optar.de>
2014-06-16 22:47:14 +0200

    im wPictureFragment: -auswahlliste für alle Files im asset Ordner. Ausgewähltes Bild anzeigen

--- nfcbeta/src/main/java/de/Beta/nfc_beta/MainActivity.java
+++ nfcbeta/src/main/java/de/Beta/nfc_beta/MainActivity.java
@@ -197,9 +197,10 @@ public class MainActivity extends ActionBarActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        // Auskommentiert,weil das nicht builded
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
         return super.onOptionsItemSelected(item);
     }
     @Override
@@ -271,6 +272,7 @@ public class MainActivity extends ActionBarActivity
                 iface.printDebugWarn("Activity Result unknown!");
                 break;
         }
+
     }
 
 
